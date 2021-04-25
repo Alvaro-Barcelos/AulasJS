a913b0d4d617d34506e8c6556cc5b2135f35ec33
unknown <alvaropassalto@gmail.com>
2021-04-25 11:53:18 -0300

    Adicionando arquivos

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,16 +1,13 @@
-
-
-
-var c = 1
+/*var c = 1
 while (c <= 10) {
     console.log(`Tudo bem? ${c}`)
     c++
 }
-
-
-
+*/
 
  var c = 1
  do {
      console.log(`Tudo bem? ${c}`)
+     c++
  } while (c <= 12)
+ 
