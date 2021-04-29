7c4bc9d0a97e6b641c53c80e5614caad78866167
unknown <alvaropassalto@gmail.com>
2021-04-29 14:24:31 -0300

    Criação de site contagem

---
+++ ExercicioSeparado/ex016/script.js
@@ -0,0 +1,7 @@
+function contar(){
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.querySelector('input#passo')
+    
+
+}
