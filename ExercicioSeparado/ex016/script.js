0564d505f7527178a112fca44769c620765ea2ad
unknown <alvaropassalto@gmail.com>
2021-05-03 21:25:54 -0300

    Salvando atualização do site contador

--- ExercicioSeparado/ex016/script.js
+++ ExercicioSeparado/ex016/script.js
@@ -1,7 +1,22 @@
-function contar(){
-    var inicio = document.querySelector('input#inicio')
-    var fim = document.querySelector('input#fim')
-    var passo = document.querySelector('input#passo')
-    
+function contar() {
+    let inicio = document.querySelector('#txti')
+    let fim = document.querySelector('#txtf')
+    let passo = document.querySelector('#txtp')
+    let dados = document.querySelector('.imprimir_dados')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERROR!!!] Favor preencher todas as caixas')
+    }
+    else{
+        dados.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
+        for(let c = i; c <= f; c += p){
+            dados.style.color = 'red'
+            dados.innerHTML += `${c} `
+            
+        }
+        
+    }
 }
